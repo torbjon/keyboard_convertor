db672c66ecf78fe4f5d44296baa5522a2f73c734
Maksim Berjoza <torbjon@gmail.com>
2013-05-30 13:31:08 +0300

    default option added, adn rubocoped

--- spec/keyboard_convertor_spec.rb
+++ spec/keyboard_convertor_spec.rb
@@ -3,23 +3,27 @@ require 'keyboard_convertor'
 
 describe 'KeyboardConveror' do
 
-  it "should convert from russian to english word [jkjlbkmybr to холодильник" do
-    '[jkjlbkmybr'.convert_keyboard({ from: 'ru', to: 'en'}).should eq('холодильник')
+  it 'should convert from russian to english word [jkjlbkmybr to холодильник' do
+    '[jkjlbkmybr'.convert_keyboard({ from: 'ru', to: 'en' }).should eq('холодильник')
   end
 
-  it "should convert from russian to english word gthtcnhjqrf1986 to перестройка1986" do
-    'gthtcnhjqrf1986'.convert_keyboard({ from: 'ru', to: 'en'}).should eq('перестройка1986')
+  it 'should convert word [jkjlbkmybr to холодильник without from/to options' do
+    '[jkjlbkmybr'.convert_keyboard().should eq('холодильник')
   end
 
-  it "should convert from russian to english word <ht;ytd Ktjybl Bkmbx to Брежнев Леонид Ильич" do
-    '<ht;ytd Ktjybl Bkmbx'.convert_keyboard({ from: 'ru', to: 'en'}).should eq('Брежнев Леонид Ильич')
+  it 'should convert from russian to english word gthtcnhjqrf1986 to перестройка1986' do
+    'gthtcnhjqrf1986'.convert_keyboard({ from: 'ru', to: 'en' }).should eq('перестройка1986')
   end
 
-  it "should convert from russian to english word \\krf to ёлка" do
-    '\krf'.convert_keyboard({ from: 'ru', to: 'en'}).should eq('ёлка')
+  it 'should convert from russian to english word <ht;ytd Ktjybl Bkmbx to Брежнев Леонид Ильич' do
+    '<ht;ytd Ktjybl Bkmbx'.convert_keyboard({ from: 'ru', to: 'en' }).should eq('Брежнев Леонид Ильич')
   end
 
-  it "should convert from russian to english word \"lefhl to Эдуард" do
+  it 'should convert from russian to english word \\krf to ёлка' do
+    '\krf'.convert_keyboard({ from: 'ru', to: 'en' }).should eq('ёлка')
+  end
+
+  it 'should convert from russian to english word "lefhl to Эдуард' do
     '"lefhl'.convert_keyboard({ from: 'ru', to: 'en' }).should eq('Эдуард')
   end
 
